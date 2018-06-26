b523825934cbe44844bd388bd8b00027c05ca31c
Chris Skyfarm <30752468+cjbear@users.noreply.github.com>
2018-06-25 19:15:17 -0700

    Changed Assignment routes, MySQL db

--- app/api/routes.py
+++ app/api/routes.py
@@ -10,7 +10,7 @@ from flask_babel import _, get_locale
 #from guess_language import guess_language
 from app import db
 #from app.api.forms import [insert relevant api/forms here ]
-from app.models import User, FyOneGoals
+from app.models import User, FyGoals
 from app.translate import translate
 from app.api import bp
 import json
@@ -23,26 +23,31 @@ from app.api import bp
 def menu():
     return render_template('api/menu.html')
 
-
 @bp.route('/fygoals', methods=['GET'])
 def fygoals():
     return render_template('api/fyGoals.html')
 
-
-@bp.route('/fygoals/<int:id>', methods=['GET'])
+@bp.route('/fygoals/<int:id>', methods=['GET', 'POST'])
 def get_fygoal(id):
-    surveyResponse = request.get_json()
-    fyGoalData = surveyResponse.json()
-    
-    FirstDraftGoal  = ['pages']['name']['page1']['elements']['name']['FirstDraftGoal']
-    startDate       = ['pages']['name']['page1']['elements']['name']['startDate']
-    endDate         = ['pages']['name']['page1']['elements']['name']['endDate']
-    specific_fy01   = ['pages']['name']['page2']['elements']['name']['specific_fy01']
-    measurable_fy01 = ['pages']['name']['page2']['elements']['name']['measurable_fy01']
-    achievable_fy01 = ['pages']['name']['page2']['elements']['name']['achievable_fy01']
-    relevant_fy01   = ['pages']['name']['page2']['elements']['name']['relevant_fy01']
-    timely_fy01     = ['pages']['name']['page2']['elements']['name']['timely_fy01']
-    finalGoal_fy01  = ['pages']['name']['page3']['elements']['name']['finalGoal_fy01']
+    if request.method == 'POST':
+        fyGoalResult = request.get_json()
+        fyGoalData = fyGoalResponse.json()
+        for key, value in fyGoalData.items():
+            if not value:
+                return jsonify({'error': 'value for {} is empty'.format(key)})
+        new_fygoal = FyGoals(
+            FirstDraftGoal  = ['pages']['name']['page1']['elements']['name']['FirstDraftGoal'],
+            startDate       = ['pages']['name']['page1']['elements']['name']['startDate'],
+            endDate         = ['pages']['name']['page1']['elements']['name']['endDate'],
+            specific_fy01   = ['pages']['name']['page2']['elements']['name']['specific_fy01'],
+            measurable_fy01 = ['pages']['name']['page2']['elements']['name']['measurable_fy01'],
+            achievable_fy01 = ['pages']['name']['page2']['elements']['name']['achievable_fy01'],
+            relevant_fy01   = ['pages']['name']['page2']['elements']['name']['relevant_fy01'],
+            timely_fy01     = ['pages']['name']['page2']['elements']['name']['timely_fy01'],
+            finalGoal_fy01  = ['pages']['name']['page3']['elements']['name']['finalGoal_fy01']
+            )
+        db.session.add(new_fygoal)
+        db.session.commit()
 
     return render_template('api/fyGoalsResults.html')
 
@@ -65,3 +70,8 @@ def create_user():
 @bp.route('/fygoal/<int:id>', methods=['PUT'])
 def update_user(id):
     pass
+
+
+@bp.route('/barriers', methods=['GET'])
+def barriers():
+    return render_template('api/barriers.html')
