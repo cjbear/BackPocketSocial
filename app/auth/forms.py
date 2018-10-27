59e1db05f57737862c4f9e67e8e71648b46ff64d
Chris Skyfarm <30752468+cjbear@users.noreply.github.com>
2018-10-27 12:42:51 -0700

    Fix registration code

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_babel import _, lazy_gettext as _l
-from app.models import User
+from app.models.User import User
 
 
 class LoginForm(FlaskForm):
@@ -22,7 +22,7 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField(_l('Register'))
 
     def validate_username(self, username):
-        user = app.User.query.filter_by(username=username.data).first()
+        user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError(_('Please use a different username.'))
 
